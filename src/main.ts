648ea4bf421c85ad66259cf758bf40c8b9085f36
antonnik15 <nikiforovkuvv@mail.ru>
2023-03-07 19:12:22 +0300

    commit init

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
-import * as cors from 'cors';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { BadRequestException, ValidationPipe } from '@nestjs/common';
+import * as cors from 'cors';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
