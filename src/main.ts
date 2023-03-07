b3375b9d13d0ab612eae24eb71247246261442e1
antonnik15 <nikiforovkuvv@mail.ru>
2023-03-07 19:16:56 +0300

    commit init

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const port = process.env.PORT || 5000;
 
-  app.use(cors());
+  app.use(cors);
   app.useGlobalPipes(
     new ValidationPipe({
       transform: true,
